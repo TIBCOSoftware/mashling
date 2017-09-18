0eee4bcb0e1f2923ed499e74732e7151c5c40392
Naresh Kumar Thota <nthota@tibco.com>
2017-09-18 11:09:12 +0530

    Added mashling version command

---
+++ cli/app/version.go
@@ -0,0 +1,52 @@
+package app
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/TIBCOSoftware/mashling/cli/cli"
+)
+
+var optVersion = &cli.OptionInfo{
+	Name:      "version",
+	UsageLine: "version",
+	Short:     "version of mashling",
+	Long:      "Displays the version of mashling",
+}
+
+//Mashling version
+const version = "1.0.2"
+
+func init() {
+	CommandRegistry.RegisterCommand(&cmdVersion{option: optVersion})
+}
+
+type cmdVersion struct {
+	option        *cli.OptionInfo
+	versionNumber string
+}
+
+// HasOptionInfo implementation of cli.HasOptionInfo.OptionInfo
+func (c *cmdVersion) OptionInfo() *cli.OptionInfo {
+	return c.option
+}
+
+// AddFlags implementation of cli.Command.AddFlags
+func (c *cmdVersion) AddFlags(fs *flag.FlagSet) {
+	// no flags
+}
+
+// Exec implementation of cli.Command.Exec
+func (c *cmdVersion) Exec(args []string) error {
+
+	if len(args) != 0 {
+		fmt.Fprintf(os.Stderr, "usage: mashling version \n\nToo many arguments given.\n")
+		os.Exit(2)
+	} else {
+		c.versionNumber = version
+		fmt.Printf("mashling version %s\n", c.versionNumber)
+	}
+
+	return nil
+}
