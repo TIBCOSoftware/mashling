a2661f3fcb3773209e796eb62d279fb8db2b3341
Naresh Kumar Thota <30426686+nareshkumarthota@users.noreply.github.com>
2017-11-01 11:33:09 +0530

    Chore gotest fix (#63)
    * Updated gotest for app_test.go
    
    * Updated gotest for gorillamux trigger

--- ext/flogo/trigger/gorillamuxtrigger/trigger_test.go
+++ ext/flogo/trigger/gorillamuxtrigger/trigger_test.go
@@ -7,15 +7,20 @@ package gorillamuxtrigger
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"testing"
 
 	"io/ioutil"
 
 	"github.com/TIBCOSoftware/flogo-lib/core/action"
+	"github.com/TIBCOSoftware/flogo-lib/core/trigger"
 )
 
-var jsonMetadata = getJsonMetadata()
+var jsonMetadata = getJSONMetadata()
 
-func getJsonMetadata() string {
+func getJSONMetadata() string {
 	jsonMetadataBytes, err := ioutil.ReadFile("trigger.json")
 	if err != nil {
 		panic("No Json Metadata found for trigger.json path")
@@ -24,22 +29,63 @@ func getJsonMetadata() string {
 }
 
 const testConfig string = `{
-  "id": "tibco-rest",
-  "ref": "github.com/TIBCOSoftware/flogo-contrib/trigger/rest",
-  "settings": {
-    "port": "8091"
-  },
-  "handlers": [
-    {
-      "actionId": "my_test_flow",
-      "settings": {
-        "method": "POST",
-        "path": "/device/:id/reset"
-      }
-    }
-  ]
-}
-`
+	 "name": "animals_rest_trigger",
+	 "id": "animals_rest_trigger",
+	 "ref": "github.com/TIBCOSoftware/mashling/ext/flogo/trigger/gorillamuxtrigger",
+	 "settings": {
+		 "port": "9096"
+	 },
+	 "output": null,
+	 "handlers": [
+		 {
+			 "actionId": "mammals_handler",
+			 "settings": {
+				 "Condition": "${trigger.content.name in (ELEPHANT,CAT)}",
+				 "autoIdReply": "false",
+				 "method": "PUT",
+				 "path": "/pets",
+				 "useReplyHandler": "false"
+			 },
+			 "output": null,
+			 "outputs": null
+		 },
+		 {
+			 "actionId": "birds_handler",
+			 "settings": {
+				 "Condition": "${trigger.content.name == SPARROW}",
+				 "autoIdReply": "false",
+				 "method": "PUT",
+				 "path": "/pets",
+				 "useReplyHandler": "false"
+			 },
+			 "output": null,
+			 "outputs": null
+		 },
+		 {
+			 "actionId": "animals_handler",
+			 "settings": {
+				 "autoIdReply": "false",
+				 "method": "PUT",
+				 "path": "/pets",
+				 "useReplyHandler": "false"
+			 },
+			 "output": null,
+			 "outputs": null
+		 },
+		 {
+			 "actionId": "animals_get_handler",
+			 "settings": {
+				 "autoIdReply": "false",
+				 "method": "GET",
+				 "path": "/pets/{petId}",
+				 "useReplyHandler": "false"
+			 },
+			 "output": null,
+			 "outputs": null
+		 }
+	 ],
+	 "outputs": null
+ }`
 
 type TestRunner struct {
 }
@@ -50,47 +96,94 @@ func (tr *TestRunner) Run(context context.Context, action action.Action, uri str
 	return 0, nil, nil
 }
 
-// func TestInitOk(t *testing.T) {
-// 	// New  factory
-// 	f := &RestFactory{}
-// 	tgr := f.New("tibco-rest")
+func TestInitOk(t *testing.T) {
+	// New  factory
+	f := &RestFactory{}
 
-// 	runner := &TestRunner{}
+	config := trigger.Config{}
+	tgr := f.New(&config)
 
-// 	config := trigger.Config{}
-// 	json.Unmarshal([]byte(testConfig), &config)
-// 	tgr.Init(config, runner)
-// }
+	runner := &TestRunner{}
 
-// func TestHandlerOk(t *testing.T) {
+	json.Unmarshal([]byte(testConfig), &config)
+	tgr.Init(runner)
+}
 
-// 	// New  factory
-// 	f := &RestFactory{}
-// 	tgr := f.New("tibco-rest")
+//Run the specified Action
+func (tr *TestRunner) RunAction(context context.Context, actionID string, inputGenerator action.InputGenerator, options map[string]interface{}) (results map[string]interface{}, err error) {
+	log.Debugf("RuAction called with actionID: %s", actionID)
+	fmt.Println("Test 2")
+	return nil, nil
+}
 
-// 	runner := &TestRunner{}
+func TestHandlerOk(t *testing.T) {
 
-// 	config := trigger.Config{}
-// 	json.Unmarshal([]byte(testConfig), &config)
-// 	tgr.Init(config, runner)
+	// New  factory
+	md := trigger.NewMetadata(jsonMetadata)
+	f := NewFactory(md)
 
-// 	tgr.Start()
-// 	defer tgr.Stop()
+	config := trigger.Config{}
+	tgr := f.New(&config)
+	runner := &TestRunner{}
+	json.Unmarshal([]byte(testConfig), &config)
 
-// 	uri := "http://127.0.0.1:8091/device/12345/reset"
+	tgr.Init(runner)
+	tgr.Start()
+	defer tgr.Stop()
 
-// 	req, err := http.NewRequest("POST", uri, nil)
+	uri := "http://localhost:9096/pets/6"
 
-// 	client := &http.Client{}
-// 	resp, err := client.Do(req)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	defer resp.Body.Close()
+	req, err := http.NewRequest("GET", uri, nil)
 
-// 	log.Debug("response Status:", resp.Status)
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	log.Debug("response Status:", resp.Status)
+	fmt.Printf("response Status: %v", resp.Status)
 
-// 	if resp.StatusCode >= 300 {
-// 		t.Fail()
-// 	}
-// }
+	if resp.StatusCode >= 300 {
+		t.Fail()
+	}
+}
+
+func BenchmarkHandlerOk(b *testing.B) {
+
+	// New  factory
+	md := trigger.NewMetadata(jsonMetadata)
+	f := NewFactory(md)
+
+	config := trigger.Config{}
+	tgr := f.New(&config)
+	runner := &TestRunner{}
+	json.Unmarshal([]byte(testConfig), &config)
+
+	tgr.Init(runner)
+	tgr.Start()
+	defer tgr.Stop()
+
+	uri := "http://localhost:9096/pets/6"
+
+	req, err := http.NewRequest("GET", uri, nil)
+
+	if err != nil {
+		panic(err)
+	}
+
+	client := &http.Client{}
+
+	for i := 0; i < b.N; i++ {
+		resp, err := client.Do(req)
+
+		if err != nil {
+			panic(err)
+		}
+		defer resp.Body.Close()
+		if resp.StatusCode >= 300 {
+			b.Fail()
+		}
+	}
+
+}
