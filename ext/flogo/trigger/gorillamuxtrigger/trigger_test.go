2518a6e3441cc5d8c3b62d5b98afc895448cd34a
Ramesh Polishetti <ramesh.polishetti@gmail.com>
2017-11-24 12:22:02 +0530

    Fix gotests

--- ext/flogo/trigger/gorillamuxtrigger/trigger_test.go
+++ ext/flogo/trigger/gorillamuxtrigger/trigger_test.go
@@ -112,6 +112,7 @@ func TestInitOk(t *testing.T) {
 
 //Run the specified Action
 func (tr *TestRunner) RunAction(ctx context.Context, act action.Action, options map[string]interface{}) (results map[string]*data.Attribute, err error) {
+	log.Debugf("Ran Action: %v", act.Config().Id)
 	return nil, nil
 }
 
