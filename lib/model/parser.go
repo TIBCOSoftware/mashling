e6cc5894b10f792fdde4816d60409d244e2923d5
Naresh Kumar Thota <30426686+nareshkumarthota@users.noreply.github.com>
2017-12-14 13:50:39 +0530

    Chore update flogomapper (#88)
    * Added metadata null check
    
    * update metadata mapper logic
    
    * Modifed default manifest
    
    * Updated tunable flow with complex mapping type

--- lib/model/parser.go
+++ lib/model/parser.go
@@ -248,11 +248,21 @@ func CreateFlogoFlowAction(handler types.EventHandler) (*faction.Config, error) 
 
 		action := actions[0]
 		action.Id = handler.Name
+
 		gatewayAction = faction.Config{
 			Id:       handler.Name,
 			Data:     action.Data,
 			Ref:      action.Ref,
 			Metadata: action.Metadata,
+		}
+
+		if gatewayAction.Metadata != nil {
+			if len(gatewayAction.Metadata.Output) == 0 {
+				gatewayAction.Metadata.Output = nil
+			}
+			if len(gatewayAction.Metadata.Input) == 0 {
+				gatewayAction.Metadata.Input = nil
+			}
 		}
 
 	} else if handler.Definition != nil {
