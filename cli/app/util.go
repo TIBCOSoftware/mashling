185fc240c026271d6b49a5183f7b0a9e88938491
nareshkumarthota <nthota@tibco.com>
2018-01-05 16:01:45 +0530

    Updated local ip code

--- cli/app/util.go
+++ cli/app/util.go
@@ -149,18 +149,11 @@ Commands:
 
 // getLocalIP gets the public ip address of the system
 func getLocalIP() string {
-	addrs, err := net.InterfaceAddrs()
+	conn, err := net.Dial("udp", "8.8.8.8:80")
 	if err != nil {
 		return "127.0.0.1"
 	}
-
-	for _, address := range addrs {
-		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
-			if ipnet.IP.To4() != nil {
-				return ipnet.IP.String()
-			}
-		}
-	}
-
-	return "127.0.0.1"
+	defer conn.Close()
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+	return localAddr.IP.String()
 }
