6192562331a346ae38e5508b101852ca8011ca44
Jeffrey Bozek <jeff.bozek@gmail.com>
2018-04-06 16:25:59 -0400

    Regen assets and shuffle shared assets.

--- internal/pkg/model/v2/schema/assets.go
+++ internal/pkg/model/v2/schema/assets.go
@@ -83,7 +83,7 @@ func schemaJson() (*asset, error) {
 		return nil, err
 	}
 
-	info := bindataFileInfo{name: "schema.json", size: 11433, mode: os.FileMode(420), modTime: time.Unix(1522965228, 0)}
+	info := bindataFileInfo{name: "schema.json", size: 11433, mode: os.FileMode(420), modTime: time.Unix(1523045791, 0)}
 	a := &asset{bytes: bytes, info: info}
 	return a, nil
 }
