21510f6cee634890c8c489d4be3b7dc048eea21a
Jeffrey Bozek <jeff.bozek@gmail.com>
2018-05-03 17:32:44 -0400

    Regen assets.

--- internal/pkg/model/v2/schema/assets.go
+++ internal/pkg/model/v2/schema/assets.go
@@ -83,7 +83,7 @@ func schemaJson() (*asset, error) {
 		return nil, err
 	}
 
-	info := bindataFileInfo{name: "schema.json", size: 11433, mode: os.FileMode(420), modTime: time.Unix(1525376543, 0)}
+	info := bindataFileInfo{name: "schema.json", size: 11433, mode: os.FileMode(420), modTime: time.Unix(1525383113, 0)}
 	a := &asset{bytes: bytes, info: info}
 	return a, nil
 }
