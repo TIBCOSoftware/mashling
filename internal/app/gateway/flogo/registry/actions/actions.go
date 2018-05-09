365c9919a80965c0037480b3d5b06f9503c4fccc
Ramesh Polishetti <ramesh.polishetti@gmail.com>
2018-05-09 15:07:08 +0530

    Generate assets

--- internal/app/gateway/flogo/registry/actions/actions.go
+++ internal/app/gateway/flogo/registry/actions/actions.go
@@ -83,7 +83,7 @@ func vendorGithubComTibcosoftwareFlogoContribActionFlowActionJson() (*asset, err
 		return nil, err
 	}
 
-	info := bindataFileInfo{name: "vendor/github.com/TIBCOSoftware/flogo-contrib/action/flow/action.json", size: 302, mode: os.FileMode(420), modTime: time.Unix(1523296852, 0)}
+	info := bindataFileInfo{name: "vendor/github.com/TIBCOSoftware/flogo-contrib/action/flow/action.json", size: 302, mode: os.FileMode(420), modTime: time.Unix(1525272079, 0)}
 	a := &asset{bytes: bytes, info: info}
 	return a, nil
 }
