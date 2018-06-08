3bc17952b9c0eba2eb1313d78457c874491503ff
Andrew Snodgrass <git@pointlander.info>
2018-06-08 13:32:26 -0600

    Integrated injectsec as service

---
+++ internal/pkg/model/v2/activity/service/sqld.go
@@ -0,0 +1,93 @@
+package service
+
+import (
+	"github.com/pointlander/injectsec"
+)
+
+// SQLD is a SQL injection attack detector
+type SQLD struct {
+	values map[string]interface{}
+	maker  *injectsec.DetectorMaker
+	Attack bool `json:"attack"`
+}
+
+// InitializeSQLD creates a SQLD service
+func InitializeSQLD(settings map[string]interface{}) (service *SQLD, err error) {
+	service = &SQLD{
+		maker: injectsec.NewDetectorMaker(),
+	}
+	err = service.UpdateRequest(settings)
+	return
+}
+
+// Execute executes the SQLD service
+func (s *SQLD) Execute() (err error) {
+	detector := s.maker.Make()
+
+	var testMap func(a map[string]interface{}) bool
+	testMap = func(a map[string]interface{}) bool {
+		for _, v := range a {
+			switch element := v.(type) {
+			case map[string]interface{}:
+				if testMap(element) {
+					return true
+				}
+			case string:
+				if detector.Detect(element) {
+					return true
+				}
+			}
+		}
+
+		return false
+	}
+
+	test := func(key string) bool {
+		if a, ok := s.values[key]; ok {
+			switch b := a.(type) {
+			case map[string]interface{}:
+				if testMap(b) {
+					s.Attack = true
+					return true
+				}
+			case map[string]string:
+				for _, v := range b {
+					if detector.Detect(v) {
+						s.Attack = true
+						return true
+					}
+				}
+			}
+		}
+
+		return false
+	}
+
+	if test("pathParams") {
+		return
+	}
+	if test("queryParams") {
+		return
+	}
+	if test("content") {
+		return
+	}
+
+	return
+}
+
+// UpdateRequest updates the SQLD service
+func (s *SQLD) UpdateRequest(values map[string]interface{}) (err error) {
+	a := values["payload"]
+	if a == nil {
+		return
+	}
+	b := *a.(*interface{})
+	c, ok := b.(map[string]interface{})
+	if !ok {
+		return
+	}
+	s.values = c
+
+	return
+}
