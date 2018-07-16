a01022055cc76ba82847a8175993beced6b2ecf1
Ramesh Polishetti <ramesh.polishetti@gmail.com>
2018-07-16 16:37:51 +0530

    Add rate limiter service

---
+++ internal/pkg/model/v2/activity/service/ratelimiter.go
@@ -0,0 +1,118 @@
+package service
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/ulule/limiter/drivers/store/memory"
+
+	"github.com/ulule/limiter"
+)
+
+var limiterInstance *limiter.Limiter
+
+// RateLimiter is a rate limiter service
+// Limit can specified in the format "<limit>-<period>"
+//
+// Valid periods:
+// * "S": second
+// * "M": minute
+// * "H": hour
+//
+// Examples:
+// * 5 requests / second : "5-S"
+// * 5 requests / minute : "5-M"
+// * 5 requests / hour : "5-H"
+type RateLimiter struct {
+	Limit string `json:"limit"`
+	Token string `json:"token"`
+
+	QuotaExceeded  bool  `json:"quotaExceeded"`
+	AvailableQuota int64 `json:"availableQuota"`
+
+	limiterInstance *limiter.Limiter
+}
+
+// Execute invokes this service
+func (rl *RateLimiter) Execute() (err error) {
+	fmt.Printf("Applying RateLimiter with %s for the request with token = %s\n", rl.Limit, rl.Token)
+
+	if rl.Token == "" {
+		fmt.Println("token not found")
+		rl.QuotaExceeded = true
+		return nil
+	}
+
+	//consume quota
+	limiterContext, err := rl.limiterInstance.Get(context.TODO(), rl.Token)
+	if err != nil {
+		return nil
+	}
+
+	//check whether rate has exceeded
+	rl.AvailableQuota = limiterContext.Remaining
+	if limiterContext.Reached {
+		rl.QuotaExceeded = true
+	} else {
+		rl.QuotaExceeded = false
+	}
+
+	return nil
+}
+
+// UpdateRequest updates a request on an existing RateLimiter service instance with new values.
+func (rl *RateLimiter) UpdateRequest(values map[string]interface{}) (err error) {
+	fmt.Println("RateLimiter -> UpdateRequest()")
+	err = rl.setRequestValues(values)
+	return err
+}
+
+// InitializeRateLimiter initializes a RateLimiter services with provided settings.
+func InitializeRateLimiter(settings map[string]interface{}) (rl *RateLimiter, err error) {
+	fmt.Println("RateLimiter -> InitializeRateLimiter()")
+	rl = &RateLimiter{}
+	err = rl.setRequestValues(settings)
+
+	//create limiter instance once
+	if limiterInstance == nil {
+		fmt.Println("creating limiterInstance")
+		//create rate
+		rate, err := limiter.NewRateFromFormatted(rl.Limit)
+		if err != nil {
+			panic(err)
+		}
+		//create store
+		store := memory.NewStore()
+		//create limiter
+		limiterInstance = limiter.New(store, rate)
+		rl.limiterInstance = limiterInstance
+	} else {
+		rl.limiterInstance = limiterInstance
+	}
+
+	return
+}
+
+func (rl *RateLimiter) setRequestValues(settings map[string]interface{}) (err error) {
+	for k, v := range settings {
+		fmt.Printf("k=%s, v=%T %v \n", k, v, v)
+		switch k {
+		case "limit":
+			limit, ok := v.(string)
+			if !ok {
+				return errors.New("Invalid type for limit")
+			}
+			rl.Limit = limit
+		case "token":
+			token, ok := v.(string)
+			if !ok {
+				rl.Token = ""
+			}
+			rl.Token = token
+		default:
+			//ignore the seting
+		}
+	}
+	return nil
+}
